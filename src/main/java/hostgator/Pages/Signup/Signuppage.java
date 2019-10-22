550b574fe1b840de77aa17ab844c1fa40d67e498
jeremyjones <jeremy.jones@endurance.com>
2019-10-22 12:33:40 -0500

    Added folding to elements and functions for Signup page for organization

--- src/main/java/hostgator/Pages/Signup/Signuppage.java
+++ src/main/java/hostgator/Pages/Signup/Signuppage.java
@@ -22,18 +22,20 @@ public class Signuppage{
 	{
 		this.driver=driver;
 	}
-
-	//Domain Section
+	//Page Elements
+	//<editor-fold desc="Domain Section">
 	By i_own_this_domain = By.id("old_domain_tab");
 	By domain_text_field = By.id("domain_field");
-	
-	//Hosting Plan
+	//</editor-fold>
+
+	//<editor-fold desc="Hosting Plan">
 	By tld_select          = By.id("tld_select");
 	By billingdropdown     = By.id("billing_cycle");
 	By username_text_field = By.xpath("//*[@id=\"username\"]");
 	By pin_text_field      = By.id("new_pin");
-	
-	// Billing Info Section
+	//</editor-fold>
+
+	//<editor-fold desc="Billing Info Section">
 	By email_text_field    = By.id("new-email");			
 	By confirm_email_field = By.id("new-email-confirm");
 	By first_name          = By.id("new-first-name");		
@@ -43,32 +45,68 @@ public class Signuppage{
 	By address2_filed      = By.id("new-address2");
 	By city_field          = By.id("new-city");
 	By zip_code_field      = By.id("zip_code");
-	
-	// Credit Card Payment Type
+	//</editor-fold>
+
+	//<editor-fold desc="Credit Card Payment Type">
 	By cc_card_name_field = By.id("new-name-on-card");
 	By cc_number_field    = By.id("new_cc");
 	By cc_cvv_field       = By.id("security_code_new");
-	
-	//Checkout Section
+	//</editor-fold>
+
+	//<editor-fold desc="Checkout Section">
 	By tos_checkbox1   = By.xpath("//*[@id=\"validation-form\"]/div[7]/div/div/div/p[3]/div/ins"); 
 	By tos_checkbox2   = By.id("tos-agree");  //By.xpath("//*[@id=\"validation-form\"]/div[7]/div/div/div/p[3]/div/ins");
 	By tos_checkbox3   = By.className("iCheck-helper"); //By.id("tos-agree");  //By.xpath("//*[@id=\"validation-form\"]/div[7]/div/div/div/p[3]/div/ins");
 	By checkout_button = By.id("final-checkout");
-	
-	//Top Right Sign in
+	//</editor-fold>
+
+	//<editor-fold desc="Top Right Sign-in">
 	By regflowTopRightEmailField     = By.id("sign-in-un");
 	By regflowTopRightPasswordField  = By.id("sign-in-pw"); 
 	By regflowTopRightSignintext     = By.linkText("Sign In!");
 	By regFlowTopRightSigninButton   = By.xpath("//*[@id=\"signup-container\"]/section/div[1]/div/div/div/div[2]/a");
-	
-	//Existing account sign in 
+	//</editor-fold>
+
+	//<editor-fold desc="Existing Account Sign-in">
 	By existingAccountPasswordField = By.id("existing-password"); 
 	By existingAccoiuntLoginButton  = By.id("section-login");
-	
-	//PayPAl
+	//</editor-fold>
+
+	//<editor-fold desc="PayPal">
 	By usePayPalTab = By.id("paypal_tab");
 	By paypal_radio        = By.id("paypal");
+	//</editor-fold>
+	//End of Page Elements
 
+	//Page Functions
+
+	//<editor-fold desc="Top Right Sign-in">
+	public void clickTopRightSignintext()
+	{
+		driver.findElement(regflowTopRightSignintext).click();
+		log.info("Clicked Top Right Sign in Button");
+	}
+
+	public void enterTopRightEmail(String email)
+	{
+		driver.findElement(regflowTopRightEmailField).sendKeys(email);
+		log.info("Entered Existing Email");
+	}
+
+	public void enterTopRightPassword(String password)
+	{
+		driver.findElement(regflowTopRightPasswordField).sendKeys(password);
+		log.info("Clicked Password");
+	}
+
+	public void clickTopRightSigninButton()
+	{
+		driver.findElement(regFlowTopRightSigninButton).click();
+		log.info("Clicked Signin Button");
+	}
+	//</editor-fold>
+
+	//<editor-fold desc="Login With Existing Account: Middle Sign in">
 	public void enterExistingAccountEmail(String email)
 	{
 		driver.findElement(email_text_field).sendKeys(email);
@@ -86,9 +124,9 @@ public class Signuppage{
 		driver.findElement(existingAccoiuntLoginButton).click();
 		log.info("Clicked on Logion Button");
 	}
-	
-	// Regflow 1
-	//Domain Section
+	//</editor-fold>
+
+	//<editor-fold desc="Choose a Domain">
 	public void clickIAlreadyOwnThisDomian()
 	{
 		driver.findElement(i_own_this_domain).click();
@@ -112,8 +150,9 @@ public class Signuppage{
 		driver.findElement(domain_text_field).sendKeys(domain+random.nextInt(10000)+packageName+random.nextInt(10000)); //random.nextInt(10000) //date.getTime());
 		log.info("Entered New Domain");
 	}
-	
-	//Hosting Plan
+	//</editor-fold>
+
+	//<editor-fold desc="Choose a Hosting Plan">
 	public void tldDropdown(int index)
 	{
 		Select s=new Select(driver.findElement(tld_select));
@@ -140,10 +179,9 @@ public class Signuppage{
 		driver.findElement(pin_text_field).sendKeys(pin);
 		log.info("Entered Pin");
 	}
-	
-	
-	// RegFlow 3
-	//// Billing Info Section
+	//</editor-fold>
+
+	//<editor-fold desc="Enter Your Billing Info">
 	public void enterEmail(String email)
 	{
 		driver.findElement(email_text_field).sendKeys(email);
@@ -198,14 +236,15 @@ public class Signuppage{
 		log.info("Entered Billing ZipCode");
 
 	}
-	
+	//</editor-fold>
+
+
 //	public void enterExistingEmail(String domain, String packageName)
 //	{
 //		driver.findElement(domain_text_field).sendKeys(domain+random.nextInt(10000)+packageName+random.nextInt(100000)+".com");
 //	}
 	
-	
-	//PayPAl
+	//<editor-fold desc="Payment Type: PayPal">
 	public void clickPayPalTab()
 	{
 		driver.findElement(usePayPalTab).click();
@@ -216,36 +255,9 @@ public class Signuppage{
 		driver.findElement(paypal_radio).click();
 		log.info("Selected Paypal Payment type");
 	}
-	
-	
-	//Top Right Sign in
-	public void clickTopRightSignintext()
-	{
-		driver.findElement(regflowTopRightSignintext).click();
-		log.info("Clicked Top Right Sign in Button");
-	}
-	
-	public void enterTopRightEmail(String email)
-	{
-		driver.findElement(regflowTopRightEmailField).sendKeys(email);
-		log.info("Entered Existing Email");
-	}
-	
-	public void enterTopRightPassword(String password)
-	{
-		driver.findElement(regflowTopRightPasswordField).sendKeys(password);
-		log.info("Clicked Password");
-	}
-	
-	public void clickTopRightSigninButton()
-	{
-		driver.findElement(regFlowTopRightSigninButton).click();
-		log.info("Clicked Signin Button");
-	}
-	
+	//</editor-fold>
 
-	// RegFlow 3
-	// Credit Card Payment Type
+	//<editor-fold desc="Payment Type: Credit Card">
 	public void enterCreditCardName(String ccName)
 	{
 		driver.findElement(cc_card_name_field).sendKeys(ccName);
@@ -263,10 +275,9 @@ public class Signuppage{
 		driver.findElement(cc_cvv_field).sendKeys(cvv);
 		log.info("Entered CC CVV");
 	}
-	
-	
-	// RegFlow 6
-	// Checkout Section
+	//</editor-fold>
+
+	//<editor-fold desc="Review Order Details and Checkout">
 	public void checkTos1()
 	{
 		driver.findElement(tos_checkbox1).click();
@@ -284,5 +295,5 @@ public class Signuppage{
 		driver.findElement(checkout_button).click();
 		log.info("Clicked Checkout Button");
 	}
-	
+	//</editor-fold>
 }
